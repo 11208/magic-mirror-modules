6078e34088f68e7bda70e682a74ac01e2420702e
Surasak Prommarat <surasak@adapterdigital.com>
2018-07-17 10:48:41 +0700

    add modules

---
+++ cp.sh
@@ -0,0 +1,3 @@
+cp -R /home/pi/magic-mirror-modules/aiclient /home/pi/MagicMirror/modules/aiclient
+cp -R /home/pi/magic-mirror-modules/aiclientdebugger /home/pi/MagicMirror/modules/aiclientdebugger
+cp /home/pi/magic-mirror-modules/config.js /home/pi/MagicMirror/config/config.js
