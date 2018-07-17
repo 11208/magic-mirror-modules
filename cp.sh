c35fcfda3ade0d59c739ba75b064cba2acd15f52
Surasak Prommarat <surasak@adapterdigital.com>
2018-07-17 10:52:28 +0700

    add modules

--- cp.sh
+++ cp.sh
@@ -1,3 +1,5 @@
+rm -rf /home/pi/MagicMirror/modules/aiclient
 cp -R /home/pi/magic-mirror-modules/aiclient /home/pi/MagicMirror/modules/aiclient
+rm -rf /home/pi/MagicMirror/modules/aiclientdebugger
 cp -R /home/pi/magic-mirror-modules/aiclientdebugger /home/pi/MagicMirror/modules/aiclientdebugger
 cp /home/pi/magic-mirror-modules/config.js /home/pi/MagicMirror/config/config.js
